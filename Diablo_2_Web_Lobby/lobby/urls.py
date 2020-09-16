468c8ba9bb8d293dc3985b3eb4a881364343aab2
savukhin <savukhin@gmail.com>
2020-09-16 10:31:04 +0300

    Added raw page of game creation

--- Diablo_2_Web_Lobby/lobby/urls.py
+++ Diablo_2_Web_Lobby/lobby/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('lobby', views.lobby, name='lobby'),
+    path('create_game', views.createGame, name='createGame'),
 ]
--- Diablo_2_Web_Lobby/lobby/views.py
+++ Diablo_2_Web_Lobby/lobby/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def lobby(request):
     return render(request, template_name='lobby.html')
+
+
+def createGame(request):
+    return render(request, template_name='createGame.html')
