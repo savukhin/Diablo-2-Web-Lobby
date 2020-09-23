7d749cd64690277131dca4fc97735dc7dee5c10d
savukhin <savukhin@gmail.com>
2020-09-23 20:57:33 +0300

    Added lock in name field when character is creating (now we can't create character with figure in name or with non-unique name)

--- Diablo_2_Web_Lobby/character/views.py
+++ Diablo_2_Web_Lobby/character/views.py
@@ -8,15 +8,19 @@ from character.PvPGNCharacter import createPvPGNCharacter
 
 
 def createCharacter(request):
+    #If 'submit' button pressed
     if (request.method == "POST"):
         characterForm = CharacterCreateForm(request.POST)
-        if characterForm.is_valid():
+        if characterForm.is_valid(): #If all fields is correct
             player = CustomUser.objects.get(user_id=request.user.id)
+            #Creating character on PvPGN server
             createPvPGNCharacter(player.user.username, request.POST['name'], request.POST['characterClass'])
+            #There is a some code to set the player in character model
             newCharacter = characterForm.save(commit=False)
             newCharacter.player = player
             newCharacter.save()
             return redirect('/')
+        return render(request, template_name='createCharacter.html', context={'form': characterForm})
 
     return render(request, template_name='createCharacter.html')
 
