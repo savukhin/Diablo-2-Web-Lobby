3fbe409c1cb64b8a81f3c45c8c806a07a8472e6a
savukhin <savukhin@gmail.com>
2020-09-24 08:02:14 +0300

    Now user must choose character before entering to the lobby

--- Diablo_2_Web_Lobby/lobby/urls.py
+++ Diablo_2_Web_Lobby/lobby/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lobby', views.lobby, name='lobby'),
+    path('lobby', views.prelobby, name='preLobby'),
+    path('lobby/char=<str:name>', views.lobby, name='lobby'),
     path('create_game', views.createGame, name='createGame'),
 ]
--- Diablo_2_Web_Lobby/lobby/views.py
+++ Diablo_2_Web_Lobby/lobby/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from character.models import Character
+from django.contrib.auth.models import User
+from authentication.models import CustomUser
 
 # Create your views here.
 
-def lobby(request):
-    return render(request, template_name='lobby.html')
+#This is a prelobby where user choose character
+def prelobby(request):
+    thisUesr = CustomUser.objects.get(user_id=request.user.id)
+    characters = Character.objects.filter(player_id=thisUesr.id)
+    return render(request, template_name='preLobby.html',
+                  context={'request': request, 'user': thisUesr, 'characters': characters})
 
 
+#This is a lobby where use choose game (character has already been chosen)
+def lobby(request, name):
+    character = Character.objects.get(name=name)
+    thisUser = CustomUser.objects.get(user_id=request.user.id)
+    if character == None: #If character not found
+        return prelobby(request)
+    if character.player != thisUser: #If character not belongs to this user
+        return prelobby(request)
+
+    return render(request, template_name='lobby.html', context={'character' : character})
+
+
+#View to page with game creating
 def createGame(request):
     return render(request, template_name='createGame.html')
