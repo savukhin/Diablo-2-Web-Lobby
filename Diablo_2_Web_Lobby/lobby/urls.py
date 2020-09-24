ae638963fdab1932c94a941ddcc5aa53391e5a0b
savukhin <savukhin@gmail.com>
2020-09-24 08:19:25 +0300

    Now user must choose character before entering to the lobby and before creating the game. Fixed some mistakes with buttons on lobby and create game pages

--- Diablo_2_Web_Lobby/lobby/urls.py
+++ Diablo_2_Web_Lobby/lobby/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('lobby', views.prelobby, name='preLobby'),
     path('lobby/char=<str:name>', views.lobby, name='lobby'),
-    path('create_game', views.createGame, name='createGame'),
+    path('create_game/char=<str:name>', views.createGame, name='createGame'),
 ]
--- Diablo_2_Web_Lobby/lobby/views.py
+++ Diablo_2_Web_Lobby/lobby/views.py
@@ -13,18 +13,31 @@ def prelobby(request):
                   context={'request': request, 'user': thisUesr, 'characters': characters})
 
 
+#Return true, if this character belongs to this user
+#IT IS NOT A VIEW!!!
+def isCharacterCorrect(thisUser, character):
+    if character == None:  # If character not found
+        return False
+    if character.player != thisUser:  # If character not belongs to this user
+        return False
+    return True
+
+
 #This is a lobby where use choose game (character has already been chosen)
 def lobby(request, name):
     character = Character.objects.get(name=name)
     thisUser = CustomUser.objects.get(user_id=request.user.id)
-    if character == None: #If character not found
-        return prelobby(request)
-    if character.player != thisUser: #If character not belongs to this user
+    if isCharacterCorrect(thisUser, character) == False: #If something wrong with this character
         return prelobby(request)
 
     return render(request, template_name='lobby.html', context={'character' : character})
 
 
-#View to page with game creating
-def createGame(request):
-    return render(request, template_name='createGame.html')
+#View to page with game creating (character has already been chosen)
+def createGame(request, name):
+    character = Character.objects.get(name=name)
+    thisUser = CustomUser.objects.get(user_id=request.user.id)
+    if isCharacterCorrect(thisUser, character) == False:  # If something wrong with this character
+        return prelobby(request)
+
+    return render(request, template_name='createGame.html', context={'character' : character})
