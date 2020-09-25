e40fa34e3d68d51fa4d8dcafb88313224e591484
savukhin <savukhin@gmail.com>
2020-09-25 11:57:40 +0300

    Added displaying an actual game list in the lobby

--- Diablo_2_Web_Lobby/lobby/D2GSConnetion.py
+++ Diablo_2_Web_Lobby/lobby/D2GSConnetion.py
@@ -1,17 +1,34 @@
 from telnetlib import Telnet
+import re
 
 
 def getGameList():
     #Telnet on 192.168.1.14:8888
     tn = Telnet("192.168.1.14", 8888)
     #Read Welcome line
-    tn.read_very_eager()
+    tn.read_until(b"Password: ")
     #Enter password (don't look on abcd123, it is default D2GS password)
     tn.write("abcd123".encode("utf-8") + b"\n")
     #Read Succeed line
-    tn.read_very_eager()
+    tn.read_until(b"D2GS>")
     #Message with game list
     tn.write("gl".encode("ascii") + b"\n")
     #Just a line with all games
-    tn.read_very_eager()
+    answer = str(tn.read_until(b"D2GS>"))
+
+    #There is an answer in table format
+
+    #No game etc
+    #| 1 name etc |
+    #| 2 name2 etc |
+
+    #So there is simple regex which find info about games (array of the each raw in table)
+    games = re.findall(r'\|[^\|]*\|', answer)
+    #Do array of splitted info in each raw (array of arrays which includes symbols '|' on the edges)
+    games = [x.split() for x in games]
+    #Get rid of symbols '|' from the right and left edges of array
+    games = [x[1:-1] for x in games]
+
+    #Closing telnet connection to avoid memory leak
     tn.close()
+    return games
--- Diablo_2_Web_Lobby/lobby/views.py
+++ Diablo_2_Web_Lobby/lobby/views.py
@@ -3,10 +3,39 @@ from lobby.D2GSConnetion import getGameList
 
 # Create your views here.
 
+#This is a n0n-database object which describes a game to show in lobby
+class Game:
+    def __init__(self, title=None, password=None, difficulty="normal", users=0):
+        self.title = title
+        self.password = password
+        self.difficulty = difficulty
+        self.users = users
+
+
 def lobby(request):
-    #To see game list in terminal
-    print("____GAME LIST_____________", getGameList(), "____GAME LIST________--")
-    return render(request, template_name='lobby.html')
+    #To see game list in terminal. There is array of splitted information about each game (array of arrays)
+    rawGameList = getGameList()
+    #There is something in format [['001', 'Www', '1', 'exp', 'sc', 'normal', 'ladder', '1', '10:42:35', 'N']]
+
+    games = []
+    for x in rawGameList:
+        title = x[1]
+        password = None
+        users = 0
+        difficulty = "normal"
+        if len(x) == 10: #That means game doesn't have a password field.
+            password = None
+            users = x[7]
+            difficulty = x[5]
+        else:
+            password = x[2]
+            users = x[8]
+            difficulty = x[6]
+
+        #Adding information about this game in list
+        games.append(Game(title, password, difficulty, users))
+
+    return render(request, template_name='lobby.html', context={'games' : games})
 
 
 def createGame(request):
