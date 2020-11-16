9c3e5bab31575d197f38f99f6e87e725422a6c21
savukhin <savukhin@gmail.com>
2020-11-16 20:37:06 +0300

    temporary reworked password hashing

--- Diablo_2_Web_Lobby/authentication/passhash.py
+++ Diablo_2_Web_Lobby/authentication/passhash.py
@@ -1,8 +1,16 @@
 import ctypes
+import urllib.request
 
 #Function that hashes password for PvPGN
 def makeHash(password):
+    url = "https://pvpgn.pro/passhash/api.php?method=hash&password=" + password
+    fp = urllib.request.urlopen(url)
+    print((fp.read()).decode('utf-8'))
+    return (fp.read()).decode('utf-8')
+'''
     test = ctypes.CDLL('authentication/bnpass.dll')
     test.pvpgn_hash.restype = ctypes.c_char_p
     test.pvpgn_hash.argtypes = [ctypes.POINTER(ctypes.c_char), ]
-    return test.pvpgn_hash(password.encode('utf-8')).decode("utf-8")
+    print(test.pvpgn_hash(password.encode('utf-8')).decode("utf-8"))
+    return test.pvpgn_hash(password.encode('utf-8')).decode("utf-8")'''
+
